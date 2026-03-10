79754bd8b0beb18629a5e2f81f8ca92fa4c9842d
Rafael Zuquim <rzuquim@gmail.com>
2026-03-09 22:46:21 -0300

    verificação se está movendo carta usando nulidade (única fonte de verdade)

--- src/Assets/Scripts/Card.cs
+++ src/Assets/Scripts/Card.cs
@@ -2,30 +2,33 @@ using UnityEngine;
 
 public partial class Card : MonoBehaviour, IClickable
 {
-    private bool isDragging;
-    private Vector2 draggingOffset;
+    private ClickEvent? dragging;
 
     public void Click(ClickEvent click)
     {
-        isDragging = true;
-        draggingOffset = click.Offset;
+        dragging = click;
         Debug.Log($"Starting to drag: {this}");
     }
 
     public void DragTo(Vector2 position)
     {
-        if (!isDragging)
+        if (!IsDragging())
         {
             Debug.LogError($"Card {nameof(DragTo)} called when not dragging");
             return;
         }
 
-        transform.position = position + draggingOffset;
+        transform.position = position + dragging.Offset;
     }
 
     public void ReleaseClick()
     {
-        isDragging = false;
+        dragging = null;
         Debug.Log($"Not dragging anymore: {this}");
+    }
+
+    private bool IsDragging()
+    {
+        return dragging is not null;
     }
 }
