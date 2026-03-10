d26c90a7c882dd8c4df22a9bfff0ac287530c181
Rafael Zuquim <rzuquim@gmail.com>
2026-03-10 11:03:32 -0300

    correção da notação de documentação do método

--- src/Assets/Scripts/UserInteraction/ClickEvent.cs
+++ src/Assets/Scripts/UserInteraction/ClickEvent.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ClickEvent
 {
-    /** Vetor de distância entre o click e o centro do objeto que foi clicado. */
+    ///<summary>Vetor de distância entre o click e o centro do objeto que foi clicado.</summary>
     public Vector2 Offset { get; }
 
     public ClickEvent(Collider2D hit, Vector3 clickPos)
