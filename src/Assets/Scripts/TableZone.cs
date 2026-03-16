570a5d445ec01720482b250f1a5e788923a18818
Rafael Zuquim <rzuquim@gmail.com>
2026-03-16 08:52:44 -0300

    adicionando zona de mesa

---
+++ src/Assets/Scripts/TableZone.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public partial class TableZone : MonoBehaviour, ICardZone
+{
+    public bool TryAdd(Card card)
+    {
+        Debug.LogWarning("TODO!");
+        return true;
+    }
+}
