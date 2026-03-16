c0c07c4bd6aea36d9ba6a57a56a091e10b77b530
Rafael Zuquim <rzuquim@gmail.com>
2026-03-16 09:14:07 -0300

    numero maximo de cartas na mao (com bug)

--- src/Assets/Scripts/PlayerHand.cs
+++ src/Assets/Scripts/PlayerHand.cs
@@ -1,10 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public partial class PlayerHand : MonoBehaviour, ICardZone
 {
+    private readonly List<Card> cards = new();
+
     public bool TryAdd(Card card)
     {
-        Debug.LogWarning("TODO!");
+        if (cards.Count >= MAX_CARDS)
+        {
+            return false;
+        }
+
+        // BUG: se colocamos uma carta aqui, tiramos e colocamos novamente ela vai estar 2x na mesma lista
+        cards.Add(card);
+        Debug.Log($"Cards in hand: {cards.Count}");
         return true;
     }
+
+    private const int MAX_CARDS = 5;
 }
--- todo/README.md
+++ todo/README.md
@@ -2,4 +2,6 @@
 
 - [x] arrastar carta pela tela com o mouse
 - [x] saber em que parte da carta foi clicado
-- [ ] regiões do tabuleiro (mão e mesa) => uma carta não pode ser colocada fora de uma região do tabuleiro
+- [x] regiões do tabuleiro (mão e mesa) => uma carta não pode ser colocada fora de uma região do tabuleiro
+- [ ] número máxima de cartas na mão
+- [ ] uma carta só pode estar em uma região ao mesmo tempo
