5649248e992f3778a54d1e39fc0f3919adedb410
Rafael Zuquim <rzuquim@gmail.com>
2026-04-03 21:51:26 -0300

    log messages on I18nLabels component

--- src/unity/Assets/Scripts/Presentation/I18n.ForSection.cs
+++ src/unity/Assets/Scripts/Presentation/I18n.ForSection.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Game.Core.Utils;
 
@@ -7,10 +8,16 @@ public partial class I18nImpl
     /// <inheritdoc/>
     public async Task<Result<I18nSection>> ForSection(string sectionKey)
     {
+        if (string.IsNullOrEmpty(sectionKey))
+        {
+            return "Cannot load i18n for an empty section key".AsResult<I18nSection>();
+        }
+
         lock (_loadedSections)
         {
             if (_loadedSections.TryGetValue(sectionKey, out var section))
             {
+                _logger.Debug?.Log($"Got i18n section from memory cache: {section}");
                 return Result.Ok(section);
             }
         }
@@ -18,11 +25,17 @@ public partial class I18nImpl
         I18nSection pristineSection;
         try
         {
+            var sw = Stopwatch.StartNew();
             pristineSection = await TryLoadSection(sectionKey);
+            _logger.Info?.Log(
+                $"Loaded pristine i18n section {sectionKey} in {sw.ElapsedMilliseconds:N2}ms"
+            );
         }
         catch (Exception ex)
         {
-            return $"Failed to load {nameof(I18n)} section '{sectionKey}': {ex}".AsResult<I18nSection>();
+            var error = $"Failed to load {nameof(I18n)} section '{sectionKey}': {ex}";
+            _logger.Error?.Log(error);
+            return error.AsResult<I18nSection>();
         }
 
         lock (_loadedSections)
@@ -39,8 +52,10 @@ public partial class I18nImpl
 
     private async Task<I18nSection> TryLoadSection(string sectionKey)
     {
-        // TODO: load i18n from disk or server
-        return new MockI18nSection();
+        // TODO: load i18n from local disk
+        // TODO: load i18n from server
+        var i18nSection = new MockI18nSection();
+        return i18nSection;
     }
 }
 
--- src/unity/Assets/Scripts/UI/I18nLabels.cs
+++ src/unity/Assets/Scripts/UI/I18nLabels.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Game.Core.Services;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -8,21 +9,34 @@ public partial class I18nLabels : MonoBehaviour
     private I18n _i18n = null!;
     private VisualElement _root = null!;
 
+    [field: Header("MANDATORY: Every label on this screen must be on this section.")]
+    [field: SerializeField]
+    public string I18nSection { get; set; } = null!;
+
     public void Awake()
     {
-        var uiDocument = Guard.NotNull(GetComponent<UIDocument>(), _logger);
-        _root = uiDocument.rootVisualElement;
-
         _logger = Service.Get<IGameLogger>();
         _i18n = Service.Get<I18n>();
+
+        Guard.NotNull(I18nSection, _logger);
+        var uiDocument = Guard.NotNull(GetComponent<UIDocument>(), _logger);
+        _root = uiDocument.rootVisualElement;
     }
 
-    // public async Task OnEnable()
-    // {
-    //     var i18nSection = await _i18n.ForSection(I18nSection);
-    //     foreach (var label in _root.Query<Label>().ToList())
-    //     {
-    //         label.text = i18nSection.Localize(label.text);
-    //     }
-    // }
+    public async Task OnEnable()
+    {
+        var i18nSection = await _i18n.ForSection(I18nSection);
+        if (!i18nSection.IsOk(out var section, out var error))
+        {
+            _logger.Error?.Log(
+                $"Could not find i18n section {i18nSection} on screen {gameObject.name}: {error}"
+            );
+            return;
+        }
+
+        foreach (var label in _root.Query<Label>().ToList())
+        {
+            label.text = section.Label(label.text);
+        }
+    }
 }
